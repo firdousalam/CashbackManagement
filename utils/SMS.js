ab89739bb26c42396d356044cb13a50e71a15e7b
firdous alam <firdous.alam2058@gmail.com>
2023-08-24 23:20:55 +0530

    Added SMS Etc

--- utils/SMS.js
+++ utils/SMS.js
@@ -1,6 +1,9 @@
-var unirest = require("unirest");
+require('dotenv').config();
+
 module.exports  = {
     sendOTP:function(mobileNo,OTP){
+
+        /*
         
         var req = unirest("GET", "https://www.fast2sms.com/dev/bulkV2");
 
@@ -24,6 +27,45 @@ module.exports  = {
 
         console.log(res.body);
         });
+        */
+
+        const https = require('https');
+        const querystring = require('querystring');
+
+        const postData = querystring.stringify({
+        'un':  process.env.MobileUserName,
+        'pwd': process.env.MobilePassword,
+        'dstno': mobileNo,
+        'msg': 'Your OTP for ABC365 Verification is : '+OTP+'. it will be expire in 5 Minutes ',
+        'type': '1',
+        'agreedterm': 'YES',
+        'sendid': '63001'
+        });
+
+        const options = {
+            hostname: 'www.isms.com.my',
+            path: '/isms_send_all_id.php',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            }
+        };
+
+        const req = https.request(options, (res) => {
+        console.log(`statusCode: `);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+        });
+
+        req.on('error', (error) => {
+            console.error(error);
+        });
+
+        req.write(postData);
+        req.end();
 
     }
 }
